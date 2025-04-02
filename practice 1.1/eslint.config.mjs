53af68e6feabcaed7fa772f5ac19aca03446c67f
Мокрецова Анна (Mokretsova_AS) <mokretsova_as@directum.com>
2025-04-02 20:27:19 +0400

    feat: настрока prettier и его применение

--- practice 1.1/eslint.config.mjs
+++ practice 1.1/eslint.config.mjs
@@ -1,15 +1,16 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import prettier from 'eslint-config-prettier';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-    languageOptions: { 
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
       // Окружение и парсеры
       globals: {
         ...globals.browser,
@@ -24,25 +25,27 @@ export default defineConfig([
           jsx: true,
         },
       },
-    } 
+    },
   },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
     // Плагины и правила
-    plugins: { 
+    plugins: {
       js,
-      'react-hooks': reactHooks
-    }, 
+      'react-hooks': reactHooks,
+    },
     rules: {
       'no-console': 'warn',
       'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
       'react-hooks/rules-of-hooks': 'error',
-      'semi': ['error', 'always'],
+      semi: ['error', 'always'],
       'react-hooks/exhaustive-deps': 'warn',
     },
-    extends: ["js/recommended"] 
+    extends: ['js/recommended'],
   },
   // Переопределения для тестовых файлов
-  { files: ['*.test.js', '*.spec.js'],
+  {
+    files: ['*.test.js', '*.spec.js'],
     rules: {
       'no-unused-expressions': 'off',
     },
