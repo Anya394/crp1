42b8a3abf89a09abe5c7c8413cba1907a3947391
anyaM <anmo_pm02@mail.ru>
2025-04-13 23:38:24 +0400

    feat: Компонент Сard и Tailwind

--- practice_3.2/src/App.tsx
+++ practice_3.2/src/App.tsx
@@ -6,6 +6,24 @@ function App() {
   return (
     <>
       <div>
+      <main>
+        <Card
+          title="TypeScript Card"
+          content="This is typed Card component."
+          sizeButton="lg"
+        />
+        <Card
+          title="Variant 2"
+          content="Notice the different button style."
+          variantButton="right"
+        />
+        <Card
+          title="Variant 3"
+          content="Notice the different button style."
+          variantButton="left"
+          sizeButton="sm"
+        />
+      </main>
     </div>
     </>
   )
---
+++ practice_3.2/src/components/Card/Card.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { CardProps } from '../../types';
+//import { Button } from '@/app/components/Button/Button';
+
+const Card: React.FC<CardProps> = ({
+  title = 'Title',
+  content = 'Content',
+  buttonText,
+  onButtonClick,
+  variantButton,
+  sizeButton
+}) => {;
+
+  return (
+    <div className="border border-[#e0e0e0] rounded-[20px] p-6 max-w-screen shadow-[0_2px_8px_rgba(38,14,59,0.1)] m-8 bg-[rgb(38,14,59)] flex flex-col items-center">
+      <h3 className='text-xl mb-3 text-[rgb(243,231,253)]'>{title}</h3>
+      <p className='text-custom-light mb-2 text-[rgb(243,231,253)]'>{content}</p>
+      
+    </div>
+  );
+};
+
+export default Card;
+/*<Button 
+        text={buttonText} 
+        onClick={onButtonClick}
+        variant={variantButton}
+        size={sizeButton}/>*/
