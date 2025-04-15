74c4c59e11b45569041c4d9bc02d9e2bf5ffba12
anyaM <anmo_pm02@mail.ru>
2025-04-15 19:35:51 +0400

    feat: project init

---
+++ practice_4.1/src/app/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import Image from "next/image";
+import React, {useEffect, useState} from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      </footer>
+    </div>
+  );
+}
